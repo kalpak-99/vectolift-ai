858925d5a896978ad48724d96ae604959d0d7dd8
kalpak-99 <harshrathore1530@gmail.com>
2026-09-04 13:06:10 +0530

    remove GoogleAnalytics component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -101,7 +101,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en">
       <body>
         {children}
-        <GoogleAnalytics gaId="G-V5FP5S9SJF" />
+       // <GoogleAnalytics gaId="G-V5FP5S9SJF" />
       </body>
     </html>
   );
